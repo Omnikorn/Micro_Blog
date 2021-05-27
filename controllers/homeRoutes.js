fa4501e01a0351cd754a95effda2525c3f1d5339
Harith Albadry <harith.albadry@gmail.com>
2021-05-27 12:34:19 +0100

    handlebars started

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,15 +5,25 @@ const withAuth = require ("../utils/auth.js")
 
 
 router.get("/", async(req,res)=> {
+   
+   try {
     const postData = await Post.findAll({ 
         include:[
             {
             model: User,
-            attributes:["user_name"]
-        }
-        ]
-    })
-})
+            attributes:["user_name"],
+        },
+        ],
+    
+}); 
 
+const posts = postData.map((post)=>post.get({plain:true}));
+res.render("homepage", {
+    posts,
+    logged_in: req.session.logged_in
+});
 
-
+} catch (err) {
+    res.status(500).json(err);
+}
+});
