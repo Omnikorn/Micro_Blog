713f15272cfae6e40bf9e36bccf2b0af938ee6de
Harith Albadry <harith.albadry@gmail.com>
2021-05-28 19:57:15 +0100

    minor adjusments

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router= require("express").Router();
 
 
 const homeRoutes = require ("./homeRoutes");
+const singlepost = require ("./homeRoutes");
 
 
 router.use("/", homeRoutes);
+router.use("/post",singlepost);
 
 module.exports= router
---
+++ controllers/singlepost.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+
+const {Post,user, User} = require("../models");
+const withAuth = require ("../utils/auth.js")
+
+
+router.get("/post/:id", async(req,res) => {
+    try{
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model:User,
+                    attributes:["user_name"]
+                },
+                {
+                    model:Comment,
+                    attributes:["comment_text","user_id"]
+                }
+            ]
+        });
+
+        const post=postData.get({plain:true});
+
+        res.render("postmain", { ...post, logged_in:req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+
+module.exports= router
