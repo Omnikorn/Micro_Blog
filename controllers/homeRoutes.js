1132969b08698817d4ebac8b1d21b6ecc6786059
Harith Albadry <harith.albadry@gmail.com>
2021-05-28 16:48:23 +0100

    minor adjusments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,6 +18,7 @@ router.get("/", async(req,res)=> {
 }); 
 
 const posts = postData.map((post)=>post.get({plain:true}));
+
 res.render("homepage", {
     posts,
     logged_in: req.session.logged_in
@@ -27,6 +28,30 @@ res.render("homepage", {
     res.status(500).json(err);
 }
 });
+
+router.get("/post/:id", async(req,res) => {
+    try{
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model:User,
+                    attributes:["user_name"]
+                },
+                {
+                    model:Comment,
+                    attributes:["comment_text","user_id"]
+                }
+            ]
+        });
+
+        const post=postData.get({plain:true});
+
+        res.render("postmain", { ...post, logged_in:req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 
 
 module.exports= router
