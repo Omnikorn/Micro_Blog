e18d57f4701f45f420420b224622e486e0064af9
Harith Albadry <harith.albadry@gmail.com>
2021-05-30 13:15:18 +0100

    login and log out

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const commentRoutes= require("./comment");
-
+const userRoutes = require("./user")
 
 router.use("/comment" , commentRoutes);
+router.use("/user",userRoutes)
 
 module.exports=router;
